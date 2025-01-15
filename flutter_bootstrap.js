4332b08d29dc684dd25e4fffe41d138997300e38
axel <mhamdan.axel@gmail.com>
2025-01-14 16:14:23 -0800

    Auto-commit on 2025-01-14 16:14:22

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"db49896cf25ceabc44096d5f088d86414e05a7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2667843844"
+    serviceWorkerVersion: "499235948"
   }
 });
